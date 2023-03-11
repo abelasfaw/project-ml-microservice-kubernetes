b8832b56b659949fc55ea5b16ec1b2de1c4f7224
EC2 Default User <ec2-user@ip-172-31-83-106.ec2.internal>
2023-03-11 20:01:02 +0000

    uploaded project files

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,19 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=abelasfawdinsa/project3
 
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run project3\
+	--image=$dockerpath\
+	--port=80 --labels app=project3
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward project3 8000:80
